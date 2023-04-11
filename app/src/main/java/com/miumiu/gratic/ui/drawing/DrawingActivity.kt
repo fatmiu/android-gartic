6fa5f3e200487ee6c94648f2d56a14c11ed01d9f
Rocky <rockyhaung@gmail.com>
2023-04-12 02:48:04 +0800

    add drawing activity layout and create custom radio button

--- app/src/main/java/com/miumiu/gratic/ui/drawing/DrawingActivity.kt
+++ app/src/main/java/com/miumiu/gratic/ui/drawing/DrawingActivity.kt
@@ -1,17 +1,40 @@
 package com.miumiu.gratic.ui.drawing
 
 import android.os.Bundle
-import android.os.PersistableBundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.miumiu.gratic.databinding.ActivityDrawingBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
-class DrawingActivity: AppCompatActivity() {
+@AndroidEntryPoint
+class DrawingActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDrawingBinding
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    private val viewModel: DrawingViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         binding = ActivityDrawingBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        subscribeToUiStateUpdates()
+
+        binding.colorGroup.setOnCheckedChangeListener { _, checkedId ->
+            viewModel.checkRadioButton(checkedId)
+        }
+    }
+
+    private fun subscribeToUiStateUpdates() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.selectedColorButtonId.collect { id ->
+                    binding.colorGroup.check(id)
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/miumiu/gratic/ui/drawing/DrawingViewModel.kt
@@ -0,0 +1,24 @@
+package com.miumiu.gratic.ui.drawing
+
+import androidx.lifecycle.ViewModel
+import com.google.gson.Gson
+import com.miumiu.gratic.R
+import com.miumiu.gratic.util.DispatcherProvider
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class DrawingViewModel @Inject constructor(
+    private val dispatcherProvider: DispatcherProvider,
+    private val gson: Gson
+) : ViewModel() {
+
+    private val _selectedColorButtonId = MutableStateFlow(R.id.rbBlack)
+    val selectedColorButtonId: StateFlow<Int> = _selectedColorButtonId
+
+    fun checkRadioButton(id: Int) {
+        _selectedColorButtonId.value = id
+    }
+}
