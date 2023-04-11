bf219af451b95fdfc9c87939566ace8722659410
Rocky <rockyhaung@gmail.com>
2023-04-11 23:26:08 +0800

    create and send clientId to server

--- app/src/main/java/com/miumiu/gratic/di/AppModule.kt
+++ app/src/main/java/com/miumiu/gratic/di/AppModule.kt
@@ -9,6 +9,8 @@ import com.miumiu.gratic.util.Constants.HTTP_BASE_URL
 import com.miumiu.gratic.util.Constants.HTTP_BASE_URL_LOCALHOST
 import com.miumiu.gratic.util.Constants.USE_LOCALHOST
 import com.miumiu.gratic.util.DispatcherProvider
+import com.miumiu.gratic.util.clientId
+import com.miumiu.gratic.util.dataStore
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -16,6 +18,7 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -35,12 +38,27 @@ object AppModule {
 
     @Singleton
     @Provides
-    fun provideOkHttpClient(): OkHttpClient {
+    fun provideOkHttpClient(clientId: String): OkHttpClient {
         return OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                val url = chain.request().url.newBuilder()
+                    .addQueryParameter("client_id", clientId)
+                    .build()
+                val request = chain.request().newBuilder()
+                    .url(url)
+                    .build()
+                chain.proceed(request)
+            }
             .addInterceptor(HttpLoggingInterceptor().apply {
                 level = HttpLoggingInterceptor.Level.BODY
             })
             .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideClientId(@ApplicationContext context: Context): String {
+        return runBlocking { context.dataStore.clientId() }
     }
 
     @Singleton
